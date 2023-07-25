4d0924519a733af5b7e35699349c5baf6769508d
CarlosMZ58 <carlosmz58ga,er@gmail.com>
2023-07-24 21:05:48 -0500

    feature bd

---
+++ src/database/masshiato.sql
@@ -0,0 +1,14 @@
+drop database if exists masshiato;
+create database masshiato;
+use masshiato;
+drop table if exists registro;
+create table registro (
+    id int primary key auto_increment, 
+    nombre varchar(30),
+    apellido varchar(30),
+    telefono varchar(30),
+    correo varchar(50),
+    contraseña varchar(30),
+    direccion varchar(50)
+);
+insert into registro(nombre, correo, contraseña)values ("admin","admin","admin");
