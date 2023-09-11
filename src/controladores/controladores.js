641d1b828fcb8c966bdd5449bda87c109362ffed
Juandz18 <juan.jo.echeverri.18@gmail.com>
2023-09-11 14:26:35 -0500

    bases//tiposcafe

--- src/controladores/controladores.js
+++ src/controladores/controladores.js
@@ -10,7 +10,9 @@ const multer = require("multer");
 controlador.zonaIndex=(consulta,respuesta)=>{
     respuesta.render("index.ejs")
 }
-
+controlador.zonaTipoCafe=(consulta,respuesta)=>{
+    respuesta.render("tiposCafe.ejs")
+}
 
 
 module.exports = controlador;
--- src/rutas/rutas.js
+++ src/rutas/rutas.js
@@ -7,7 +7,7 @@ const cargaImagenes = multer({
 const controlador = require('../controladores/controladores')
 
 rutas.get('/',controlador.zonaIndex);
-
+rutas.get('/tipoCafe',controlador.zonaTipoCafe);
 
 
 module.exports = rutas
