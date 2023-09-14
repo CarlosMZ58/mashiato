b3f1a5092ca324b4906468216dac08b0c5531e02
Juandz18 <juan.jo.echeverri.18@gmail.com>
2023-09-14 11:42:03 -0500

    navegacion // terminada (falta barra de busqueda)

--- src/public/static/js/style.js
+++ src/public/static/js/style.js
@@ -10,10 +10,10 @@ document.getElementById('input').addEventListener('change', ()=>{
 window.addEventListener("scroll", function () {
   const navbar = document.querySelector("nav");
   if (window.scrollY > 30) { // Cambia 50 a la cantidad de desplazamiento que desees antes de que la barra cambie de color
-      navbar.style.backgroundColor = "#2e1f18";
+      navbar.style.backgroundColor = "#493226";
       navbar.style.height = "60px"; 
   } else {
-      navbar.style.backgroundColor = "#2e1f18";
+      navbar.style.backgroundColor = "#493226";
       navbar.style.height = "90px";
     }
 });
