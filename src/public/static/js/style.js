2e5f249b7c8662d1bf7d6a207a85b838f4ef14cf
Juandz18 <juan.jo.echeverri.18@gmail.com>
2023-09-21 21:47:45 -0500

    navegacion merge2

--- src/public/static/js/style.js
+++ src/public/static/js/style.js
@@ -1,31 +1,3 @@
-let slider = document.querySelector("#sliderContenedorPresentacion")
-let sliderIndividual = document.querySelectorAll(".contenidoSliderPre")
-let contador = 1;
-let width = sliderIndividual[0].clientWidth;
-let intervalo = 5200;
-
-window.addEventListener("resize", function(){
-  width = sliderIndividual[0].clientWidth;
-
-})
-
-setInterval(function(){
-  slides();
-}, intervalo);
-
-function slides (){
-  slider.style.transform = "translate("+(-width*contador)+"px)";
-  slider.style.transition = "transform 1s";
-  contador++; 
-
-  if(contador == sliderIndividual.length){
-    setTimeout(function(){
-      slider.style.transform = "translate(0px)";
-      slider.style.transition = "transform 0s";
-      contador=1;
-    },2600)
-  }
-}
 document.getElementById('input').addEventListener('change', ()=>{
   if(document.body.className.indexOf('dark') ===-1) {
     document.body.classList.add('dark');
