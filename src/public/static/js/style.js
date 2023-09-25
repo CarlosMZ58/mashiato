ec394a991242b9b521ef722a0da66f8fac84315c
Juandz18 <juan.jo.echeverri.18@gmail.com>
2023-09-25 17:01:01 -0500

    navegacion lateral casi terminada

--- src/public/static/js/style.js
+++ src/public/static/js/style.js
@@ -4,5 +4,13 @@ listElememts.forEach(listElememt => {
   listElememt.addEventListener('click', ()=>{
     
     listElememt.classList.toggle('arrow');
+    
+    let height = 0;
+    let menu = listElememt.nextElementSibling;
+    console.log(menu.scrollHeight)
+    if(menu.clientHeight == "0"){
+      height=menu.scrollHeight;
+    }
+    menu.style.height = height+"px";
   })
 });
